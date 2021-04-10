9b6fc1735b00b82f22768728e0d5613ee875aff9
Nikola Bojovic <58517620+nikolabojovic97@users.noreply.github.com>
2021-04-10 20:35:31 +0200

    Routing and new theme

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgbDropdownModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ],
+  exports: [
+    NgbDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
